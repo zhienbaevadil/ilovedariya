0833b9ab6bcebd22452dd0e7351c68ca850dce5b
heroofhumanity <159687250+zhienbaevadil@users.noreply.github.com>
2025-12-29 02:30:42 +0500

    Add files via upload

---
+++ page10/script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+  document.querySelector(".content").classList.add("show");
+});
+
+document.querySelector(".end-btn").addEventListener("click", () => {
+  window.location.href = "../page6/index.html";
+});
